1bb76e08c3fee65f983138505d7c0b26dadfabc1
李政宇 <1153643666@qq.com>
2023-09-20 20:33:23 +0800

    修改登录页面

--- web/src/views/AboutView.vue
+++ web/src/views/AboutView.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <button>测试</button><up-circle-outlined /><play-circle-outlined />
   </div>
+
 </template>
+<script setup>
+</script>
