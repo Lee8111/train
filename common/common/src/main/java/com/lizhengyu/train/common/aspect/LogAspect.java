677a63867fd19a3195d5bf9258a1d3043d567a60
李政宇 <1153643666@qq.com>
2023-09-20 12:18:15 +0800

    增加公共的模块Common，用来放置公共的类，例如工具类，拦截器，AOP，常量等

--- common/common/src/main/java/com/lizhengyu/train/common/aspect/LogAspect.java
+++ common/common/src/main/java/com/lizhengyu/train/common/aspect/LogAspect.java
@@ -1,4 +1,4 @@
-package com.lizhengyu.train.member.aspect;
+package com.lizhengyu.train.common.aspect;
 
 import cn.hutool.core.util.RandomUtil;
 import com.alibaba.fastjson.JSONObject;
@@ -22,13 +22,11 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.multipart.MultipartFile;
 
 
-
-
 @Aspect
 @Component
 public class LogAspect {
     public LogAspect(){
-        System.out.println("LogAspect");
+        System.out.println("Common LogAspect");
     }
 
     private final static Logger LOG = LoggerFactory.getLogger(LogAspect.class);
