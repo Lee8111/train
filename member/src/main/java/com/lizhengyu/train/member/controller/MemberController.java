4db4cbf5ba1560cb63cd5335ed9a4b74b42c17bf
李政宇 <1153643666@qq.com>
2023-09-20 15:49:22 +0800

    实现一个简单的注册接口

--- member/src/main/java/com/lizhengyu/train/member/controller/MemberController.java
+++ member/src/main/java/com/lizhengyu/train/member/controller/MemberController.java
@@ -3,6 +3,7 @@ package com.lizhengyu.train.member.controller;
 import com.lizhengyu.train.member.service.MemberService;
 import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,9 +14,15 @@ public class MemberController {
     @Resource
     private MemberService memberService;
     @GetMapping("/count")
-    public int count(){
+    public Integer count(){
 
         return memberService.count();
+
+    }
+    @PostMapping("/register")
+    public long register(String mobile){
+
+        return memberService.register(mobile);
 
     }
 }
