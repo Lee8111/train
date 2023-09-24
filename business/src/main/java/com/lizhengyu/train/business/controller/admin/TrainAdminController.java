38ff8c7d7e4aa1ade8dcb8d44b510c10d0be1683
李政宇 <1153643666@qq.com>
2023-09-24 22:43:57 +0800

    火车车次下拉框开发，前端页面加后端接口

--- business/src/main/java/com/lizhengyu/train/business/controller/admin/TrainAdminController.java
+++ business/src/main/java/com/lizhengyu/train/business/controller/admin/TrainAdminController.java
@@ -1,15 +1,16 @@
 package com.lizhengyu.train.business.controller.admin;
 
-import com.lizhengyu.train.common.context.LoginMemberContext;
-import com.lizhengyu.train.common.resp.CommonResp;
-import com.lizhengyu.train.common.resp.PageResp;
 import com.lizhengyu.train.business.req.TrainQueryReq;
 import com.lizhengyu.train.business.req.TrainSaveReq;
 import com.lizhengyu.train.business.resp.TrainQueryResp;
 import com.lizhengyu.train.business.service.TrainService;
+import com.lizhengyu.train.common.resp.CommonResp;
+import com.lizhengyu.train.common.resp.PageResp;
 import jakarta.annotation.Resource;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/admin/train")
@@ -34,6 +35,11 @@ public class TrainAdminController {
     public CommonResp<Object> delete(@PathVariable Long id) {
         trainService.delete(id);
         return new CommonResp<>();
+    }
+    @GetMapping("/query-all")
+    public CommonResp<List<TrainQueryResp>> queryAll() {
+        List<TrainQueryResp> list = trainService.queryAll();
+        return new CommonResp<>(list);
     }
 
 }
