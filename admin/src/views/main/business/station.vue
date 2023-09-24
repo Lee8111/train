2f4b783840188acb19686216daab61b47bc5baab
李政宇 <1153643666@qq.com>
2023-09-24 22:06:11 +0800

    为火车和火车站增加拼音

--- admin/src/views/main/business/station.vue
+++ admin/src/views/main/business/station.vue
@@ -92,6 +92,9 @@ export default defineComponent({
       if(Tool.isNotEmpty(station.value.name)){
         station.value.namePinyin=pinyin(station.value.name,{toneType:'none'}).replace(" ","");
         station.value.namePy=pinyin(station.value.name,{pattern:'first',toneType:'none'}).replace(" ","");
+      }else{
+        station.value.namePinyin="";
+        station.value.namePy="";
       }
     },{immediate:true});
 
