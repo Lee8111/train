9db2fc9c623e58a6f03f3281b1716badc29df916
李政宇 <1153643666@qq.com>
2023-09-25 21:24:56 +0800

    添加quartz定时任务的内容

---
+++ batch/src/main/java/com/lizhengyu/train/batch/job/TestJob.java
@@ -0,0 +1,13 @@
+package com.lizhengyu.train.batch.job;
+
+import org.quartz.DisallowConcurrentExecution;
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+@DisallowConcurrentExecution
+public class TestJob implements Job {
+    @Override
+    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        System.out.println("Test111");
+    }
+}
