96684d83a8c01e0d7d27c042c2e276eeab2c6073
李政宇 <1153643666@qq.com>
2023-09-26 15:43:26 +0800

    增加每日生成车次功能

--- business/src/main/java/com/lizhengyu/train/business/controller/admin/DailyTrainAdminController.java
+++ business/src/main/java/com/lizhengyu/train/business/controller/admin/DailyTrainAdminController.java
@@ -1,15 +1,17 @@
 package com.lizhengyu.train.business.controller.admin;
 
-import com.lizhengyu.train.common.context.LoginMemberContext;
-import com.lizhengyu.train.common.resp.CommonResp;
-import com.lizhengyu.train.common.resp.PageResp;
 import com.lizhengyu.train.business.req.DailyTrainQueryReq;
 import com.lizhengyu.train.business.req.DailyTrainSaveReq;
 import com.lizhengyu.train.business.resp.DailyTrainQueryResp;
 import com.lizhengyu.train.business.service.DailyTrainService;
+import com.lizhengyu.train.common.resp.CommonResp;
+import com.lizhengyu.train.common.resp.PageResp;
 import jakarta.annotation.Resource;
 import jakarta.validation.Valid;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
 
 @RestController
 @RequestMapping("/admin/daily-train")
@@ -35,5 +37,9 @@ public class DailyTrainAdminController {
         dailyTrainService.delete(id);
         return new CommonResp<>();
     }
-
+    @GetMapping("/gen-daily/{date}")
+    public CommonResp<Object> genDaily(@PathVariable @DateTimeFormat(pattern = "yyyy-MM-dd") Date date) {
+        dailyTrainService.genDaily(date);
+        return new CommonResp<>();
+    }
 }
