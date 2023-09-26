7ff8657ddd9f7677c11b28cb9bce68b63f5f0d1b
李政宇 <1153643666@qq.com>
2023-09-26 22:03:35 +0800

    生成车次时，同步生成余票信息

--- business/src/main/java/com/lizhengyu/train/business/service/DailyTrainService.java
+++ business/src/main/java/com/lizhengyu/train/business/service/DailyTrainService.java
@@ -39,6 +39,8 @@ public class DailyTrainService {
     private DailyTrainCarriageService dailyTrainCarriageService;
     @Resource
     private DailyTrainSeatService dailyTrainSeatService;
+    @Resource
+    private DailyTrainTicketService dailyTrainTicketService;
 
     public void save(DailyTrainSaveReq req) {
         DateTime now = DateTime.now();
@@ -121,6 +123,9 @@ public class DailyTrainService {
 
         //生成该车次的车座数据
         dailyTrainSeatService.genDaily(date,train.getCode());
+
+        //生成该车次的余票数据
+        dailyTrainTicketService.genDaily(date,train.getCode());
 
         LOG.info("生成日期[{}]车次【{}】的信息结束", DateUtil.formatDate(date));
     }
